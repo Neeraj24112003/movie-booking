a9c9468dc2fb9082b4dd059b3bd6ea0376726a12
Neeraj <neeraj.temp@example.com>
2026-01-06 15:27:34 +0530

    chore: enable CORS for production client url

--- server/index.js
+++ server/index.js
@@ -19,7 +19,11 @@ app.use(express.json());
 
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174'],
+    origin: [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        process.env.CLIENT_URL // Add your Vercel URL here via environment variable
+    ].filter(Boolean),
     credentials: true
 }));
 app.use(cookieParser());
